8009391c655d31e1596cb011e804fb379b5ef42b
CharulBhanawat13 <charulbhanawat@ymail.com>
2020-04-01 15:20:13 +0530

    Added  menu

--- app/src/main/java/com/spirograph/MainActivity.java
+++ app/src/main/java/com/spirograph/MainActivity.java
@@ -1,12 +1,18 @@
 package com.spirograph;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.spirograph.shapes.Line;
 
@@ -28,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
     EditTextCollection lengthsEditText = new EditTextCollection();
     EditTextCollection angleIncrementsEditTexts = new EditTextCollection();
 
+    Boolean starClicked = Boolean.FALSE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState   );
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("  " + "SpiroGraph");
@@ -42,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         resumeButton = findViewById(R.id.resumeButton);
         restartButton = findViewById(R.id.restartButton);
         submitButton = findViewById(R.id.submitButton);
-
         dynamicEditTexts = findViewById(R.id.dynamicEditTexts);
 
         linearLayout = findViewById(R.id.linearLayout);
@@ -68,6 +75,47 @@ public class MainActivity extends AppCompatActivity {
                         angleIncrementsEditTexts
                 )
         );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.invite_friend:  try {
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,"This message is from application under testing(AUT).Application is in the stage of maturity.PLEASE IGNORE \n"
+                );
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "SMS failed, please try again later!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.help:
+                Toast.makeText(getApplicationContext(),"Under Development!!!",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.star:
+                Toast.makeText(getApplicationContext(),"Star clicked",Toast.LENGTH_LONG).show();
+                starClicked = !starClicked;
+                if (starClicked)
+                item.setIcon(R.drawable.ic_star_full_white);
+                else
+                    item.setIcon(R.drawable.ic_star_white);
+
+                List<Integer> lengths=lengthsEditText.getLengths();
+                List<Integer> intAngleIncrements = angleIncrementsEditTexts.getLengths();
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void submitButtonOnClick(View view) {
